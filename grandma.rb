8218446b3f1fa6bbf715c00f60d385afbcd2e7e4
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-11 01:46:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,22 @@
+def speak_to_grandma(phrase)
+  if phrase = "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  if phrase = "Hi Nana, how are you?"
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
